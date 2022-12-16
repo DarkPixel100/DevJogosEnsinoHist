17fdad3e31dfc354899cb98a900b0cd93988a774
DarkPixel100 <diegofavila20@gmail.com>
2022-12-16 00:21:10 -0300

    Started adding jumping sound (no smoke particles)

--- Assets/Scripts/Player/CharBaseMov.cs
+++ Assets/Scripts/Player/CharBaseMov.cs
@@ -147,6 +147,7 @@ public class CharBaseMov : MonoBehaviour
     public void Jump()
     {
         GetComponent<PlayerParticles>().CreateDust();
+        GetComponent<AudioSource>().Play();
         jumpPress = false;
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(new Vector2(0, jumpForce * 100));
